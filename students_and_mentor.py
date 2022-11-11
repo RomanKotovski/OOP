a3bedd91cda0a2986a87fcd4e5970cebd75f2cdf
Kotovski <roman_kotovski@mail.ru>
2022-11-11 22:09:41 +0500

    first task

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -12,6 +12,11 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+class Lecturer(Mentor):
+    pass
+class Reviewer(Mentor):
+    pass
         
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
