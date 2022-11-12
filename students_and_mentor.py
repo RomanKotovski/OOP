71dd7329044ad250084c5343a216c9b402d05dbc
Kotovski <roman_kotovski@mail.ru>
2022-11-12 22:05:42 +0500

    second task

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -7,17 +7,26 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         
+    def rate_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.courses_attached = []
+        self.courses_attached                                                                                                                                                                                                                           = []
 
 class Lecturer(Mentor):
-    pass
-class Reviewer(Mentor):
-    pass
-        
+    def __init__(self):
+        self.grades = {}
+    
+class Reviewer(Mentor):   
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -30,7 +39,7 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
  
 cool_mentor.rate_hw(best_student, 'Python', 10)
